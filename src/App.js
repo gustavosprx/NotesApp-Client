cdcdbb792540621afe188bb9ea5649b0b5a32a6e
gustavosprx <gustavo.sprx@gmail.com>
2021-06-07 18:01:34 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import Register from '../src/components/Register'
+import Login from '../src/components/Login'
+import Home from '../src/components/Home'
+import Navbar from '../src/components/Navbar'
+import Index from '../src/components/Index'
+import { BrowserRouter, Route } from 'react-router-dom'
+import NoteState from './context/notes/noteState'
+import AlertState from './context/alerts/alertState'
+import AuthState from './context/auth/authState'
+import authToken from './config/tokenAuth'
+import PrivateRoute from './components/PrivateRoute'
+
+
+// Revisar si tengo un token
+const token = localStorage.getItem('token')
+if(token) {
+  authToken(token)
+}
+
 
 function App() {
+
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <AlertState>
+        <AuthState>
+          <NoteState>
+            <BrowserRouter>
+                <Navbar />
+                <Route path="/" component={Index} exact />
+                <PrivateRoute path="/home"  component={Home} exact />                   
+                <Route path='/register' component={Register} exact />
+                <Route path='/login' component={Login} exact />
+                                      
+            </BrowserRouter>
+          </NoteState>
+        </AuthState>  
+      </AlertState>
     </div>
   );
 }
